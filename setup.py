6061effc2ffd2361211e5507478c7d7ff0f664ac
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2021-10-26 17:33:25 +0200

    At least gets installed

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/asiaszmek/channelunit/issues",
     },
     package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+
     python_requires=">=3.6",
     include_package_data=False,
     license='LGPL-2.1-or-later',
