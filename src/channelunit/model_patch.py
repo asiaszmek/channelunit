34ceb3898daaa64724236637dd472e73c4f54735
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2021-10-26 18:00:53 +0200

    Making an object works

--- src/channelunit/model_patch.py
+++ src/channelunit/model_patch.py
@@ -28,36 +28,35 @@ class ModelPatch(sciunit.Model,
 
     def compile_and_add(self):
         working_dir = os.getcwd()
-        os.chdir(mechanisms_path)
+        os.chdir(self.mod_path)
         p = run('nrnivmodl')
-        neuron.load_mechanisms(mechanisms_path)
+        neuron.load_mechanisms(self.mod_path)
         os.chdir(working_dir)
 
     def __init__(self, path_to_mods, channel_name,
                  gbar_name="gbar", temp=22, E_rev=None):
         self.mod_path = path_to_mods
         self.compile_and_add()
-        self.soma = h.Section(name="soma",
-                              cell="my_patch")
+        self.soma = h.Section(name="soma")
         self.soma.L = 1
         self.soma.diam = 1
         self.soma.insert("extracellular")
         self.channel = self.soma.insert(channel_name)
         #set up channel conductance/permeability in case it is 0
-        if self.soma.psection()["density_mechs"][channel_name][gbar] == 0:
+        if self.soma.psection()["density_mechs"][channel_name][gbar_name] == 0:
             for seg in self.soma:
                 from_mech = getattr(seg, channel_name)
                 gbar_val = 0.001
                 setattr(from_mech, gbar, gbar_val)
         self.temperature = temp
-        self.vclamp = h.SEClamp(soma(0.5))
+        self.vclamp = h.SEClamp(self.soma(0.5))
         
         E_rev_name = self.get_E_rev_name()        
         if E_rev is None:
             if E_rev_name is None:
                 raise SystemExit('Unable to proceed, if E_rev is unknown.')
             else:
-                val_E_rev = self.get_E_rev_val()
+                val_E_rev = self.get_E_rev_value()
                 if val_E_rev is not None:
                     self.E_rev = val_E_rev
                 else:
