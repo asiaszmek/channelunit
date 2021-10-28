16a9663f63285c4ab79f0070a2605f1dc7c01c4e
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2021-10-28 12:26:04 +0200

    Write observations to a dict
    This will be changed -- it's better to have a dict with a list of values,
    where the first one is the observation and the second one is the error/std.

--- src/channelunit/tests/test_steady_state.py
+++ src/channelunit/tests/test_steady_state.py
@@ -8,7 +8,7 @@ class SteadyStateTest(Test):
     """
     Common features of the Activation Steady State and Inactivation steady state
     """
-    score_type = scores.ZScore_SteadyStateCurves
+    #score_type = scores.ZScore_SteadyStateCurves
     def extract_stimulation(self, observation_dict):
         stim_list = []
         for key in observation_dict.keys():
@@ -19,35 +19,47 @@ class SteadyStateTest(Test):
                     print("observation %s is not a float" % key)
                     continue
                 stim_list.append(float(key))
-            elif isinstance(key, float):
-                stim_list.append(key)
-            elif isinstance(key, int):
+            elif isinstance(key, float) or isinstance(key, int):
                 stim_list.append(key)
         return stim_list
 
     def format_data(self, observation_dict):
-        pass
+        observation_std = {}
+        observation = {}
+        for (key, val) in observation_dict:
+            if isinstance(key, str):
+                new_key = float(key)
+            elif isinstance(key, float):
+                new_key = key
+            elif isinstance(key, int):
+                new_key = key
+            else:
+                continue
+            if isinstance(val, str):
+                values = val.split(" ")
+                new_val = float(values[0])
+                if len(values) == 2:
+                    std_val = float(values[1])
+                    observation_std[new_key] = std_val
+            elif isinstance(val, float) or isinstance(val, int):
+                new_val = val
+            observation[new_key] = new_val
+            if new_key not in observation_std:
+                observation_std[new_key] = 0.03
+        return observation, observation_std
+
     
 class ActivationSteadyStateTest(SteadyStateTest):
 
     def __init__(self, observation, name="Activation Steady State Test",
                  base_directory="", show_figures=True):
 
-        self.injections = self.extract_stimulation(observation)
-        observation = self.format_data(observation)
-        observation = self.add_std_to_observation(observation)
+        self.injections = self.extract_stimulation(observation) 
+        observation_vals, observation_stds = self.format_data(observation)
         Test.__init__(self, observation, name)
         self.required_capabilities += (NModlChannel)
         self.base_directory = base_directory
         self.show_figures = show_figures
         self.npool = multiprocessing.cpu_count() - 1
 
-    def format_data(self, observation):
-        new_observation = {}
-        for (key, val) in observation:
-            if isinstance(str, val):
-                values = val.split(" ")
-                new_observation[key] = values[0]
-                if len(values) == 2:
-                    key_std = "std_" + str(key)
-                    new_observation[key_std] = 
+       
