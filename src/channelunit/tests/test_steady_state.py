fa0a8dd940fe6ebe17ba8ec044b3430685d1eb81
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2021-12-17 14:28:00 +0100

    Add a distinction between electrode_current and channel_current (Ba experiments)

--- src/channelunit/tests/test_steady_state.py
+++ src/channelunit/tests/test_steady_state.py
@@ -126,19 +126,20 @@ class ActivationSteadyStateTest(SteadyStateTest):
         except:
             raise SystemExit("chord_conductance not specified in experimental conditions")
         try:
-            self.leak_subtraction = experimental_conditions["leak_subtraction"]
+            self.channel_current = experimental_conditions["channel_current"]
         except KeyError:
-            self.leak_subtraction = True
+            raise SystemExit("It must be specified in experimental condition if it is channel_current")
+
         self.power = power
         self.observation = OrderedDict(sorted(self.observation.items()))
         self.stimulus_list = self.extract_stimulation(self.observation)
 
     def run_model(self, model, stim_list, v_init, t_stop,
-                  power, chord_conductance, leak_subtraction):
+                  power, chord_conductance, channel_current):
         return model.get_activation_steady_state(stim_list,
                                                  v_init, t_stop, power,
                                                  chord_conductance,
-                                                 leak_subtraction)
+                                                 channel_current)
     
 
     def generate_prediction(self, model, verbose=False):
@@ -146,7 +147,7 @@ class ActivationSteadyStateTest(SteadyStateTest):
         prediction = self.run_model(model, self.stimulus_list, self.v_init,
                                     self.t_stop, self.power,
                                     self.chord_conductance,
-                                    self.leak_subtraction)
+                                    self.channel_current)
         if self.save_figures:
             name = self.name.replace(" ", "_")
             self.generate_figures(model, self.observation, prediction,
@@ -177,19 +178,19 @@ class InactivationSteadyStateTest(SteadyStateTest):
         except KeyError:
             raise SystemExit("chord_conductance not specified in experimental conditions")
         try:
-            self.leak_subtraction = experimental_conditions["leak_subtraction"]
+            self.channel_current = experimental_conditions["channel_current"]
         except KeyError:
-            self.leak_subtraction = True
+              raise SystemExit("It must be specified in experimental condition if it is channel_current")    
         self.power = power
         self.observation = OrderedDict(sorted(self.observation.items()))
         self.stimulus_list = self.extract_stimulation(self.observation)
 
     def run_model(self, model, stim_list, v_test, t_test, power,
-                  chord_conductance, leak_subtraction):
+                  chord_conductance, channel_current):
         return model.get_inactivation_steady_state(stim_list,
                                                    v_test, t_test, power,
                                                    chord_conductance,
-                                                   leak_subtraction)
+                                                   channel_current)
     
 
     def generate_prediction(self, model, verbose=False):
@@ -197,7 +198,7 @@ class InactivationSteadyStateTest(SteadyStateTest):
         prediction = self.run_model(model, self.stimulus_list, self.v_test,
                                     self.t_test, self.power,
                                     self.chord_conductance,
-                                    self.leak_subtraction)
+                                    self.channel_current)
         if self.save_figures:
             name = self.name.replace(" ", "_")
             self.generate_figures(model, self.observation, prediction,
