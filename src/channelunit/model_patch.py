2edf063a78518098da8bce53277cea992e9cb507
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2022-01-17 10:55:57 +0100

    Test leak subtraction

--- src/channelunit/model_patch.py
+++ src/channelunit/model_patch.py
@@ -17,11 +17,31 @@ mechanisms_path = os.path.join(loc, 'mechanisms')
 F = 96485.33212  # C mol^-1
 R = 8.314462618  # J mol^-1 K^-1
 
-
-class ModelPatch(sciunit.Model, NModlChannel):
-    _nai = 10  # mM
-    _ki = 140  # mM
-    _cai = 100e-6  # mM
+class ModelPatch(sciunit.Model):
+    def __init__(self, temp=22, R_m=20000,
+                 v_rest=-65, liquid_junction_pot=0,
+                 cvode=True,
+                 sim_dt=0.001):
+        h.load_file("stdrun.hoc")
+        self.dt = 0.01
+        self.compile_and_add(mechanisms_path, True)
+        self.junction = liquid_junction_pot
+        self.patch = h.Section(name="patch")
+        self.patch.L = 1
+        self.patch.Ra = 100
+        self.patch.diam = 1
+        self.patch.insert("pas")
+        self.patch.e_pas = v_rest
+        self.patch.g_pas = 1/R_m
+        self.temperature = temp
+        self.v_low = 50
+        self.vclamp = h.SEClampOLS(self.patch(0.5))
+        self.cvode = cvode
+        if cvode:
+            h.CVode()
+            h.CVode().atol(1e-5)
+        else:
+            h.dt = sim_dt
         
     def compile_and_add(self, path, recompile):
         working_dir = os.getcwd()
@@ -31,6 +51,114 @@ class ModelPatch(sciunit.Model, NModlChannel):
         neuron.load_mechanisms(path)
         os.chdir(working_dir)
 
+    def set_vclamp(self, dur1, v1, dur2, v2, leak_subtraction, delay=200):
+        self.vclamp.dur1 = dur1
+        self.vclamp.amp1 = v1 - self.junction
+        self.vclamp.dur2 = dur2
+        self.vclamp.amp2 = v2 - self.junction
+        if not leak_subtraction:
+            self.vclamp.dur3 = 0
+            self.vclamp.dur4 = 0
+            self.vclamp.dur5 = 0
+            self.vclamp.dur6 = 0
+            self.vclamp.dur7 = 0
+            self.vclamp.dur8 = 0
+            self.vclamp.dur9 = 0
+            self.vclamp.dur10 = 0
+            self.vclamp.dur11 = 0
+            self.vclamp.dur12 = 0
+            return dur1+dur2+delay
+
+        pulse_amp = self.vclamp.amp2 - self.vclamp.amp1
+        self.vclamp.dur3 = delay
+        self.vclamp.amp3 = self.vclamp.amp1
+        v_sub = self.vclamp.amp1 - pulse_amp/4 - self.v_low - self.junction
+        v_pulse = v_sub + pulse_amp/4 
+        self.vclamp.amp4 = v_sub
+        self.vclamp.dur4 = delay
+        # 1st pulse
+        self.vclamp.amp5 = v_pulse
+        self.vclamp.dur5 = dur2
+        self.vclamp.amp6 = v_sub
+        self.vclamp.dur6 = delay
+        #2nd pulse
+        self.vclamp.amp7 = v_pulse
+        self.vclamp.dur7 = dur2
+        self.vclamp.amp8 = v_sub
+        self.vclamp.dur8 = delay
+        #3rd pulse
+        self.vclamp.amp9 = v_pulse
+        self.vclamp.dur9 = dur2
+        self.vclamp.amp10 = v_sub
+        self.vclamp.dur10 = delay
+        #4th pulse
+        self.vclamp.amp11 = v_pulse
+        self.vclamp.dur11 = dur2
+        self.vclamp.amp12 = v_sub
+        self.vclamp.dur12 = delay
+        return dur1 + 6*delay + 5*dur2
+
+    def run(self, t_stop):
+        current = h.Vector()
+        current.record(self.vclamp._ref_i, self.dt)
+        if self.cvode:
+            h.CVode().re_init()
+        else:
+            h.init()
+        h.tstop = t_stop
+        h.run()
+        return current.as_numpy()
+
+class ModelPatchWithChannel(ModelPatch, NModlChannel):
+    _nai = 10  # mM
+    _ki = 140  # mM
+    _cai = 100e-6  # mM
+    def __init__(self, path_to_mods, channel_name, ion_name,
+                 external_conc=None, gbar_name="gbar", temp=22,
+                 recompile=True, liquid_junction_pot=0,
+                 cvode=True, R_m=20000, v_rest=-65, gbar_value=None,
+                 directory="validation_results",  sim_dt=0.001):
+        """
+        ion_name: str
+            most common ions are: na (sodium), k, ca (sometimes Ca, if 
+            your model has different Ca pools). 
+            if you specify nonspecific, you need to provide a value of E_rev
+            It is important to pay attention whether the ion variable is 
+            specified with a lowercase or an uppercase letter, because 
+            the name of the reversal potential variable is constructed
+            based on the ion name (na -> ena, k -> ek, ca -> eca, Ca -> eCa).
+        R_m: float 
+            mebrane resistivity (in ohm*cm^2)
+        """
+        
+        self.channel_name = channel_name
+        self.mod_path = path_to_mods
+        self.compile_and_add(self.mod_path, recompile)
+        super(ModelPatchWithChannel, self).__init__(temp=temp, 
+                                                    R_m=R_m, v_rest=v_rest,
+                                                    liquid_junction_pot=liquid_junction_pot,
+                                                    cvode=cvode,
+                                                    sim_dt=sim_dt,)
+
+        self.channel = self.patch.insert(self.channel_name)
+        self.base_directory = directory
+        #set up channel conductance/permeability in case it is 0
+        chan = self.patch.psection()["density_mechs"][channel_name]
+        self.gbar_name = gbar_name
+        self.ion_name = ion_name
+        self._external_conc = external_conc
+        if gbar_name not in chan.keys():
+            raise SystemExit('Unable to proceed, unknown %s conductance (gbar)'
+                             % channel_name)
+
+        if chan[gbar_name][0] == 0 or gbar_value is not None:
+            for seg in self.patch:
+                from_mech = getattr(seg, channel_name)
+                if gbar_value is None:
+                    gbar_value = 0.001
+                setattr(from_mech, gbar_name, gbar_value)
+        self.ca = None
+        
     @property
     def gbar(self):
         mech = self.patch.psection()["density_mechs"][self.channel_name]
@@ -95,114 +223,7 @@ class ModelPatch(sciunit.Model, NModlChannel):
         conc_fact = np.log(self._external_conc/internal)
         E_rev = 1e3*R*(273.15+self.temperature)/(valence*F)*conc_fact
         return E_rev
-
-
-    def __init__(self, path_to_mods, channel_name, ion_name,
-                 external_conc=None, gbar_name="gbar", temp=22,
-                 recompile=True, liquid_junction_pot=0,
-                 cvode=True, R_m=20000, v_rest=-65, gbar_value=None,
-                 directory="validation_results",  sim_dt=0.001):
-        """
-        ion_name: str
-            most common ions are: na (sodium), k, ca (sometimes Ca, if 
-            your model has different Ca pools). 
-            if you specify nonspecific, you need to provide a value of E_rev
-            It is important to pay attention whether the ion variable is 
-            specified with a lowercase or an uppercase letter, because 
-            the name of the reversal potential variable is constructed
-            based on the ion name (na -> ena, k -> ek, ca -> eca, Ca -> eCa).
-        R_m: float 
-            mebrane resistivity (in ohm*cm^2)
-        """
-        h.load_file("stdrun.hoc")
-        self.dt = 0.01
-        self.channel_name = channel_name
-        self.mod_path = path_to_mods
-        self.compile_and_add(mechanisms_path, recompile)
-        self.compile_and_add(self.mod_path, recompile)
-        self.patch = h.Section(name="patch")
-        self.patch.L = 1
-        self.patch.Ra = 100
-        self.patch.diam = 1
-        self.patch.insert("pas")
-        self.patch.e_pas = v_rest
-        self.patch.g_pas = 1/R_m
-        self.channel = self.patch.insert(self.channel_name)
-        self.junction = liquid_junction_pot
-        self.base_directory = directory
-        #set up channel conductance/permeability in case it is 0
-        chan = self.patch.psection()["density_mechs"][channel_name]
-        self.gbar_name = gbar_name
-        self.ion_name = ion_name
-        self._external_conc = external_conc
-        if gbar_name not in chan.keys():
-            raise SystemExit('Unable to proceed, unknown %s conductance (gbar)'
-                             % channel_name)
-
-        if chan[gbar_name][0] == 0 or gbar_value is not None:
-            for seg in self.patch:
-                from_mech = getattr(seg, channel_name)
-                if gbar_value is None:
-                    gbar_value = 0.001
-                setattr(from_mech, gbar_name, gbar_value)
-        self.temperature = temp
-        self.v_low = 50
-        self.vclamp = h.SEClampOLS(self.patch(0.5))
-        self.cvode = cvode
-        if cvode:
-            h.CVode()
-            h.CVode().atol(1e-5)
-        else:
-            h.dt = sim_dt
-        self.ca = None
-          
-    def set_vclamp(self, dur1, v1, dur2, v2, leak_subtraction, delay=200):
-        self.vclamp.dur1 = dur1
-        self.vclamp.amp1 = v1 - self.junction
-        self.vclamp.dur2 = dur2
-        self.vclamp.amp2 = v2 - self.junction
-        if not leak_subtraction:
-            self.vclamp.dur3 = 0
-            self.vclamp.dur4 = 0
-            self.vclamp.dur5 = 0
-            self.vclamp.dur6 = 0
-            self.vclamp.dur7 = 0
-            self.vclamp.dur8 = 0
-            self.vclamp.dur9 = 0
-            self.vclamp.dur10 = 0
-            self.vclamp.dur11 = 0
-            self.vclamp.dur12 = 0
-            return dur1+dur2+delay
-
-        pulse_amp = self.vclamp.amp2 - self.vclamp.amp1
-        self.vclamp.dur3 = delay
-        self.vclamp.amp3 = self.vclamp.amp1
-        v_sub = self.vclamp.amp1 - pulse_amp/4 - self.v_low - self.junction
-        v_pulse = v_sub + pulse_amp/4 
-        self.vclamp.amp4 = v_sub
-        self.vclamp.dur4 = delay
-        # 1st pulse
-        self.vclamp.amp5 = v_pulse
-        self.vclamp.dur5 = dur2
-        self.vclamp.amp6 = v_sub
-        self.vclamp.dur6 = delay
-        #2nd pulse
-        self.vclamp.amp7 = v_pulse
-        self.vclamp.dur7 = dur2
-        self.vclamp.amp8 = v_sub
-        self.vclamp.dur8 = delay
-        #3rd pulse
-        self.vclamp.amp9 = v_pulse
-        self.vclamp.dur9 = dur2
-        self.vclamp.amp10 = v_sub
-        self.vclamp.dur10 = delay
-        #4th pulse
-        self.vclamp.amp11 = v_pulse
-        self.vclamp.dur11 = dur2
-        self.vclamp.amp12 = v_sub
-        self.vclamp.dur12 = delay
-        return dur1 + 6*delay + 5*dur2
-
+   
     def get_activation_traces(self, stimulation_levels: list,
                               v_hold: float, t_stop:float,
                               chord_conductance=False,
@@ -264,7 +285,8 @@ class ModelPatch(sciunit.Model, NModlChannel):
             ca_fname = "%s_calcium" % fname
             calcium_vals = []
             calcium = h.Vector()
-            calcium.record(self.ca[self.memb_shell].nodes[0]._ref_concentration, self.dt)
+            calcium.record(self.ca[self.memb_shell].nodes[0]._ref_concentration,
+                           self.dt)
         else:
             ca_fname = ""
         h.celsius = self.temperature
@@ -630,7 +652,7 @@ class ModelPatch(sciunit.Model, NModlChannel):
         return new_current
 
 
-class ModelPatchNernst(ModelPatch):    
+class ModelPatchNernst(ModelPatchWithChannel):    
     def __init__(self, path_to_mods, channel_name, ion_name,
                  external_conc=None, gbar_name="gbar", temp=22, recompile=True,
                  liquid_junction_pot=0, cvode=True, R_m=20000, v_rest=-65,
@@ -705,7 +727,7 @@ class ModelPatchNernst(ModelPatch):
 
 
     
-class ModelPatchConcentration(ModelPatch):
+class ModelPatchConcentration(ModelPatchWithChannel):
     def __init__(self, path_to_mods, channel_name, ion_name,
                  external_conc=None, gbar_name="gbar", temp=22, recompile=True,
                  liquid_junction_pot=0, cvode=True, R_m=20000, v_rest=-65,
