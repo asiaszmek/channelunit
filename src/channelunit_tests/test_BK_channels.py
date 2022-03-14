f211297a1a6b521b6bb1d0bbb5728beac4649690
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2022-03-14 15:29:46 +0100

    Add BK activation curves for other Ca concentrations

--- src/channelunit_tests/test_BK_channels.py
+++ src/channelunit_tests/test_BK_channels.py
@@ -11,8 +11,12 @@ from channelunit import data_path
 channel_loc = os.path.join(data_path, "ion_channels")
 activation_loc_BK_1_uM_Ca = os.path.join(data_path, "data",
                                          "I_BKCa_1_uM_Ca.csv")
+activation_loc_BK_10_uM_Ca = os.path.join(data_path, "data",
+                                         "I_BKCa_10_uM_Ca.csv")
+activation_loc_BK_100_uM_Ca = os.path.join(data_path, "data",
+                                         "I_BKCa_100_uM_Ca.csv")
 
-class TestBK_CaV21(unittest.TestCase):
+class TestBK_1_uM_Ca(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = ModelCaConcClamp(channel_loc,
@@ -32,7 +36,7 @@ class TestBK_CaV21(unittest.TestCase):
         cls.power = 1
         cls.test = ActivationSteadyStateTest(cls.activation_data,
                                              {"v_init": -80, "t_stop": 40,
-                                              "electrode_current": True,
+                                              "electrode_current": False,
                                               "chord_conductance":False,
                                               "normalization": "to_one"},
                                              1,
@@ -62,5 +66,106 @@ class TestBK_CaV21(unittest.TestCase):
         self.assertTrue(is_all_less_1)
 
 
+class TestBK_10_uM_Ca(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = ModelCaConcClamp(channel_loc,
+                                     ["bk"],
+                                     ["k", "ca"],
+                                     gbar_values={"bk": 1},
+                                     external_conc={"k": 5},
+                                     internal_conc={"ca": 10e-3,
+                                                    "k": 120},
+                                     temp=23,
+                                     ljp=0)
+        activation_data = np.loadtxt(activation_loc_BK_10_uM_Ca, skiprows=1,
+                                     delimiter=",")
+        cls.activation_data = {}
+        for val in activation_data:
+            cls.activation_data[val[0]] = val[1:].tolist()
+        cls.power = 1
+        cls.test = ActivationSteadyStateTest(cls.activation_data,
+                                             {"v_init": -80, "t_stop": 40,
+                                              "electrode_current": False,
+                                              "chord_conductance":False,
+                                              "normalization": "to_one"},
+                                             1,
+                                             "ActvationTestBKCa10uMCa",
+                                             save_figures=True)
+        cls.act_results = cls.test.run_model(cls.model,
+                                             cls.test.stimulus_list,
+                                             cls.test.v_init,
+                                             cls.test.t_stop,
+                                             cls.power,
+                                             cls.test.chord_conductance,
+                                             cls.test.electrode_current,
+                                             "to_one")
+       
+
+    def test_summarize(self):
+        self.score = self.test.judge(self.model)
+        self.score.summarize()
+
+    def test_run_model_keys(self):
+        self.assertEqual(list(self.act_results.keys()),
+                         self.test.stimulus_list)
+
+    def test_run_model_values(self):
+        values = np.array(list(self.act_results.values()))
+        is_all_less_1 = np.all((values<=1))
+        self.assertTrue(is_all_less_1)
+
+
+class TestBK_100_uM_Ca(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = ModelCaConcClamp(channel_loc,
+                                     ["bk"],
+                                     ["k", "ca"],
+                                     gbar_values={"bk": 1},
+                                     external_conc={"k": 5},
+                                     internal_conc={"ca": 100e-3,
+                                                    "k": 120},
+                                     temp=23,
+                                     ljp=0)
+        activation_data = np.loadtxt(activation_loc_BK_100_uM_Ca, skiprows=1,
+                                     delimiter=",")
+        cls.activation_data = {}
+        for val in activation_data:
+            cls.activation_data[val[0]] = val[1:].tolist()
+        cls.power = 1
+        cls.test = ActivationSteadyStateTest(cls.activation_data,
+                                             {"v_init": -80, "t_stop": 40,
+                                              "electrode_current": False,
+                                              "chord_conductance":False,
+                                              "normalization": "to_one"},
+                                             1,
+                                             "ActvationTestBKCa100uMCa",
+                                             save_figures=True)
+        cls.act_results = cls.test.run_model(cls.model,
+                                             cls.test.stimulus_list,
+                                             cls.test.v_init,
+                                             cls.test.t_stop,
+                                             cls.power,
+                                             cls.test.chord_conductance,
+                                             cls.test.electrode_current,
+                                             "to_one")
+       
+
+    def test_summarize(self):
+        self.score = self.test.judge(self.model)
+        self.score.summarize()
+
+    def test_run_model_keys(self):
+        self.assertEqual(list(self.act_results.keys()),
+                         self.test.stimulus_list)
+
+    def test_run_model_values(self):
+        values = np.array(list(self.act_results.values()))
+        is_all_less_1 = np.all((values<=1))
+        self.assertTrue(is_all_less_1)
+
+
+        
 if __name__ == "__main__":
     unittest.main()
