922dcc6767ff588414e37e65722d3490f3fe4b0a
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2022-03-25 14:42:33 +0100

    Test generating figs with Act/Inact curves

---
+++ src/channelunit/tests/test_act_inact_steady_state_at_once.py
@@ -0,0 +1,146 @@
+import os
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+from sciunit import Test, Score
+
+
+from channelunit.capabilities import NModlChannel
+from .test_steady_state import InactivationSteadyStateTest
+from .test_steady_state import ActivationSteadyStateTest
+from channelunit.scores import ZScore_BothSteadyStateCurves
+
+class SteadyStateTest(Test):
+    """
+    Test if steady state activation curve and steady state inactivation
+    curves of the model channel fit experimental data
+
+    observation: a dictonary with "Activation" and "Inactivation" keys. 
+          Values are dictionaries of experimental observations in the form:
+          key: applied voltage, value: activation value
+    """
+    def __init__(self, observation: dict, experimental_conditions: dict,
+                 name, electrode_current, normalization,
+                 power={"Activation": 1, "Inactivation": 1},
+                 base_directory="", save_figures=True):
+        act_obs = observation["Activation"]
+        act_cond = experimental_conditions["Activation"]
+        act_cond["electrode_current"] = electrode_current
+        act_cond["normalization"] = normalization
+
+        inact_obs = observation["Inactivation"]
+        inact_cond = experimental_conditions["Inactivation"]
+        inact_cond["electrode_current"] = electrode_current
+        inact_cond["normalization"] = normalization
+        Test.__init__(self, observation, name)
+        self.electrode_current = electrode_current
+        self.power = power
+        self.normalization = normalization
+        self.required_capabilities += (NModlChannel,)
+        self.base_directory = base_directory
+        self.save_figures = save_figures
+        self.score_type = ZScore_BothSteadyStateCurves
+        self.dpi = 200
+     
+        self.act_test = ActivationSteadyStateTest(act_obs, act_cond,
+                                                  power["Activation"],
+                                                  "%s_%s" %(name,
+                                                            "Activation"),
+                                                  base_directory,
+                                                  save_figures=False)
+        
+        self.inact_test = InactivationSteadyStateTest(inact_obs, inact_cond,
+                                                      power["Inactivation"],
+                                                      "%s_%s" % (name,
+                                                                 "Inactivation"),
+                                                    base_directory,
+                                                    save_figures=False)
+        
+    def run_model(self, model, act_stim_list, act_v_init, act_t_stop,
+                  act_power, act_chord_conductance, inact_stim_list,
+                  inact_v_test, inact_t_test,
+                  inact_power,  inact_chord_conductance,
+                  electrode_current, normalization):
+        act_prediction = model.get_activation_SS(act_stim_list,
+                                                 act_v_init, act_t_stop,
+                                                 act_power,
+                                                 act_chord_conductance,
+                                                 electrode_current,
+                                                 normalization,
+                                                 save_traces=False,
+                                                 save_ca=False)
+        inact_prediction = model.get_inactivation_SS(inact_stim_list,
+                                                     inact_v_test,
+                                                     inact_t_test,
+                                                     inact_power,
+                                                     inact_chord_conductance,
+                                                     electrode_current,
+                                                     normalization,
+                                                     save_traces=False,
+                                                     save_ca=False)
+        return {"Activation": act_prediction,
+                "Inactivation": inact_prediction}
+    
+
+    def generate(self, model, verbose=False):
+        prediction = self.run_model(model, self.act_test.stimulus_list,
+                                    self.act_test.v_init,
+                                    self.act_test.t_stop,
+                                    self.act_test.power,
+                                    self.act_test.chord_conductance,
+                                    self.inact_test.stimulus_list,
+                                    self.inact_test.v_test,
+                                    self.inact_test.t_test,
+                                    self.inact_test.power,
+                                    self.inact_test.chord_conductance,
+                                    self.electrode_current,
+                                    self.normalization)
+        return prediction
+
+
+    def generate_figures(self, model, observations, predictions, name):
+        label = ""
+        for n in model.channel_names:
+            label += n + " "
+        channel_names_path = ""
+        for channel_name in model.channel_names:
+            channel_names_path += channel_name + "_"
+
+        if self.base_directory:
+            path = os.path.join(self.base_directory, "figs",
+                                channel_names_path)
+        else:
+            path = os.path.join(model.base_directory, "figs",
+                                channel_names_path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+
+        fig, ax = plt.subplots(1, 1)
+        
+        act_v = list(observations["Activation"].keys())
+        act_p = [predictions["Activation"][v] for v in act_v]
+        act_vals = [observations["Activation"][v][0] for v in act_v]
+        act_vals_std = [observations["Activation"][v][1] for v in act_v]
+        
+        self.add_to_fig(act_v, act_p, act_val1, act_vals_std,
+                        ax, label, marker="d",
+                        ylabel="Normalized conductance")
+        
+        ax2 = ax.twinx()
+        inact_v = list(observations["Inactivation"].keys())
+        inact_p = [predictions["Inactivation"][v] for v in inact_v]
+        inact_vals = [observations["Inactivation"][v][0] for v in inact_v]
+        inact_vals_std = [observations["Inactivation"][v][1] for v in inact_v]
+        
+        self.add_to_fig(inact_v, inact_p, inact_val1, inact_vals_std,
+                        ax2, label, marker="d",
+                        ylabel="Normalized current")
+        ymax = max(max(ax.get_ylim()), max(ax2.get_ylim()))
+        ymin = min(min(ax.get_ylim()), min(ax2.get_ylim()))
+        ax.set_ylim([ymin, ymax])
+        ax2.set_ylim([ymin, ymax])
+                   
+        savefig_path = os.path.join(path, "Steady_state_%s_%s.png"
+                                    % (channel_names_path, name))
+        fig.savefig(savefig_path, dpi=self.dpi,
+                    bbox_inches='tight')
---
+++ src/channelunit_tests/test_2_curves.py
@@ -0,0 +1,73 @@
+import os
+import unittest
+
+import numpy as np
+
+from channelunit.tests import SteadyStateTest
+from channelunit import ModelWholeCellPatchSingleChan
+from channelunit import data_path
+
+
+channel_loc = os.path.join(data_path, "ion_channels")
+activation_loc = os.path.join(data_path, "data",
+                              "I_Na_activation.csv")
+inactivation_loc = os.path.join(data_path, "data",
+                                "I_Na_inactivation.csv")
+
+class TestSteadyState(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = ModelWholeCellPatchSingleChan(channel_loc, "na3", "na",
+                                                  external_conc=110,
+                                                  temp=22,
+                                                  ljp=0)
+        
+        activation_data = np.loadtxt(activation_loc, skiprows=1,
+                                     delimiter=",")
+        inactivation_data = np.loadtxt(inactivation_loc, skiprows=1,
+                                       delimiter=",")
+        cls.activation_data = dict(val.tolist() for val in activation_data)
+         
+        cls.inactivation_data = dict(val.tolist() for val in inactivation_data)
+        
+        cls.data = {"Activation": cls.activation_data,
+                    "Inactivation": cls.inactivation_data}
+        cls.experimental_conditions = {"Activation": {"v_init": -90,
+                                                      "t_stop": 200,
+                                                      "chord_conductance":
+                                                      True},
+                                       "Inactivation": {"v_test": -5,
+                                                        "t_test": 10,
+                                                        "chord_conductance":
+                                                        False}
+        }
+        
+        
+        
+        cls.test = SteadyStateTest(cls.data, cls.experimental_conditions,
+                                   "Na3SS", electrode_current=False,
+                                   normalization="to_one")
+        
+    def test_summarize(self):
+        self.score = self.test.judge(self.model)
+        self.score.summarize()
+
+
+    def test_run_model(self):
+        out = self.test.run_model(self.model, self.test.act_test.stimulus_list,
+                                    self.test.act_test.v_init,
+                                    self.test.act_test.t_stop,
+                                    self.test.act_test.power,
+                                    self.test.act_test.chord_conductance,
+                                    self.test.inact_test.stimulus_list,
+                                    self.test.inact_test.v_test,
+                                    self.test.inact_test.t_test,
+                                    self.test.inact_test.power,
+                                    self.test.inact_test.chord_conductance,
+                                    self.test.electrode_current,
+                                    self.test.normalization)
+        self.assertEqual(list(out.keys()), ["Activation", "Inactivation"])
+
+        
+if __name__ == "__main__":
+    unittest.main()
