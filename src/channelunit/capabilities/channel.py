e1b3c95eba2855845b5c5c3f7d175a42e88fa46c
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2022-03-30 11:28:21 +0200

    Forgot to rename those methods

--- src/channelunit/capabilities/channel.py
+++ src/channelunit/capabilities/channel.py
@@ -5,21 +5,21 @@ class NModlChannel(sciunit.Capability):
     """This is an abstract model class providing capabilities
     that must be implemented by the patch model class.
     """
-    def get_activation_steady_state(self, stimulation_levels: list,
-                                    v_init: float, t_stop:float,
-                                    chord_conductance=False,
-                                    electrode_current=True,
-                                    sim_dt=0.001, normalization="to_one"):
+    def get_activation_SS(self, stimulation_levels: list,
+                          v_init: float, t_stop:float,
+                          chord_conductance=False,
+                          electrode_current=True,
+                          sim_dt=0.001, normalization="to_one"):
         """This function must be implemented by the patch model class.
         """
 
         raise NotImplementedError()
 
-    def get_inactivation_steady_state(self, stimulation_levels: list,
-                                      v_test: float, t_test:float,
-                                      chord_conductance=False,
-                                      electrode_current=True,
-                                      sim_dt=0.001, normalization="to_one"):
+    def get_inactivation_SS(self, stimulation_levels: list,
+                            v_test: float, t_test:float,
+                            chord_conductance=False,
+                            electrode_current=True,
+                            sim_dt=0.001, normalization="to_one"):
         """This function must be implemented by the patch model class.
         """
 
